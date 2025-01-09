b9e94b417e0500378446626f225913798374cb34
micas2002 <miguelbernardinosousa2002@gmail.com>
2025-01-09 15:51:47 +0000

    finished ex02

--- Module03/ex02/sources/main.cpp
+++ Module03/ex02/sources/main.cpp
@@ -1,28 +1,38 @@
+#include <vector>
 #include "Shape.hpp"
 #include "Rectangle.hpp"
 #include "Circle.hpp"
 #include "Triangle.hpp"
 
 int	main() {
-	{
+		std::vector<Shape*>	shapes;
 		Rectangle rectangle = Rectangle(5, 3);
-
-		std::cout << rectangle.calculateArea() << std::endl;
-		std::cout << rectangle.calculatePerimeter() << std::endl;
-	}
-
-	{
 		Circle circle = Circle(3);
-
-		std::cout << circle.calculateArea() << std::endl;
-		std::cout << circle.calculatePerimeter() << std::endl;
-	}
-
-	{
 		Triangle triangle = Triangle(4, 3, 5, 3);
 
+		std::cout << "rectangle" << std::endl;
+		std::cout << rectangle.calculateArea() << std::endl;
+		std::cout << rectangle.calculatePerimeter() << std::endl;
+
+
+		std::cout << "circle" << std::endl;
+		std::cout << circle.calculateArea() << std::endl;
+		std::cout << circle.calculatePerimeter() << std::endl;
+
+
+		std::cout << "triangle" << std::endl;
 		std::cout << triangle.calculateArea() << std::endl;
 		std::cout << triangle.calculatePerimeter() << std::endl;
-	}
+
+
+		std::cout << "Liskov Substitution Principle" << std::endl;
+		shapes.push_back(&rectangle);
+		shapes.push_back(&circle);
+		shapes.push_back(&triangle);
+
+		std::cout << rectangle.calculatePerimeter() << std::endl;
+		std::cout << circle.calculatePerimeter() << std::endl;
+		std::cout << triangle.calculatePerimeter() << std::endl;
+		
 	return (0);
 }
