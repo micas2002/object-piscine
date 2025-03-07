40a65095ebdb9443b3f1500ad7acb04748db9b83
micas2002 <miguelbernardinosousa2002@gmail.com>
2025-03-07 17:28:30 +0000

    secretary is now a factory of forms.(need to check if implementation using switch case is allowed instead of using interfaces)

--- Module04 - Design Pattern/ex02 - Factory and Command/sources/Person.cpp
+++ Module04 - Design Pattern/ex02 - Factory and Command/sources/Person.cpp
@@ -111,8 +111,22 @@ Secretary&	Secretary::operator=(const Secretary& assign) {
 }
 
 Form*	Secretary::createForm(FormType p_formType) {
-	(void)p_formType;
-	return (nullptr);
+	switch (p_formType) {
+		case FormType::CourseFinished:
+			return new CourseFinishedForm();
+		
+		case FormType::NeedMoreClassRoom:
+			return new NeedMoreClassRoomForm();
+		
+		case FormType::NeedCourseCreation:
+			return new NeedCourseCreationForm();
+		
+		case FormType::SubscriptionToCourse:
+			return new SubscriptionToCourseForm();
+		
+		default:
+			return (nullptr);
+	}
 }
 
 void	Secretary::archiveForm() {}
