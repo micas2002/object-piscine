c702abd743c558bcad689eb48a246ac849e85938
micas2002 <miguelbernardinosousa2002@gmail.com>
2025-03-14 17:49:44 +0000

    added condition to prevent execution of form with no name(needs revision)

--- Module04 - Design Pattern/ex02 - Factory and Command/sources/FormCommand.cpp
+++ Module04 - Design Pattern/ex02 - Factory and Command/sources/FormCommand.cpp
@@ -5,7 +5,7 @@ Receiver::Receiver() {}
 Receiver::~Receiver() {}
 
 void	Receiver::executeForm(Form* p_form) {
-	if (p_form->getIsSigned() == false) {
+	if (p_form->getIsSigned() == false || p_form->getWhoCreatedForm() == "") {
 		std::cout << "Form can't be executed, it has not been signed by the Headmaster!" << std::endl;
 		return;
 	}
